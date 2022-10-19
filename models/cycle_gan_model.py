ab1906deffdb0970839253331f2b5d942cb03d81
TJ <tjchase34@gmail.com>
2022-10-19 12:16:23 -0400

    Modifying model + options for wrapper compatability

--- models/cycle_gan_model.py
+++ models/cycle_gan_model.py
@@ -96,7 +96,7 @@ class CycleGANModel(BaseModel):
             self.optimizers.append(self.optimizer_G)
             self.optimizers.append(self.optimizer_D)
 
-    def set_input(self, input):
+    def set_input(self, source, target):
         """Unpack input data from the dataloader and perform necessary pre-processing steps.
 
         Parameters:
@@ -105,9 +105,11 @@ class CycleGANModel(BaseModel):
         The option 'direction' can be used to swap domain A and domain B.
         """
         AtoB = self.opt.direction == 'AtoB'
-        self.real_A = input['A' if AtoB else 'B'].to(self.device)
-        self.real_B = input['B' if AtoB else 'A'].to(self.device)
-        self.image_paths = input['A_paths' if AtoB else 'B_paths']
+        # self.real_A = input['A' if AtoB else 'B'].to(self.device)
+        # self.real_B = input['B' if AtoB else 'A'].to(self.device)
+        # self.image_paths = input['A_paths' if AtoB else 'B_paths']
+        self.real_A = source.to(self.device)
+        self.real_B = target.to(self.device)
 
     def forward(self):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
